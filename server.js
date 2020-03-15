deadc36fccd31f6d7babc2ee7322f2ed2c3c0e3c
mikejeter <mjeter45@yahoo.com>
2020-03-15 19:21:52 -0400

    Added get/favicon to remove 404 error in browser console

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const postRouter = require('./posts/post-router.js');
 
+
 const server = express();
 
 server.use(express.json());
 server.use('/api/posts', postRouter);
 
-server.get('/favicon.ico', (req, res) => {
-    res.status(204);
-});
+
 // endpoints
 
 server.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+server.get('/favicon.ico', (req, res) => {
+    res.status(204);
 });
 
 
