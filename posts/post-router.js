4f2b76f408e4e6ff594f2b0106eb39c8a9a16f7f
mikejeter <mjeter45@yahoo.com>
2020-03-17 21:37:09 -0400

    Writing endpoints and error responses

--- posts/post-router.js
+++ posts/post-router.js
@@ -12,29 +12,68 @@ router.get('/', (req, res) => {
         res.status(200).json({post});
     })
     .catch(err => {
-    res.status(500).json({success:false, err});
+    res.status(500).json({success:false, err: 'The posts information could not be retrieved.'});
     });  
+});
+
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    db.findById(postId)
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist.'});
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({err: 'The post information could not be retrieved.'});
+        });
+});
+
+router.get('/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    db.findPostComments(postId)
+        .then(comments => {
+            if (comments.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist.'});
+            } else {
+                res.status(200).json(comments);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err: 'The comments information could not be retrieved.'});
+        });
 });
 
 // C Create (CRUD)
 router.post('/', (req, res) => {
-    const postInfo = req.body;
+    const { title, contents } = req.body;
 
-    db.insert(postInfo)
+    db.insert(req.body)
         .then(post => {
-            res.status(201).json({success:true, post})
+            if (!title || !contents) {
+                res.status(400).json({ message: 'Please provide title and contents for the post.'});
+            } else {
+                res.status(201).json(post);
+            }
         })
         .catch(err => {
-            res.status(500).json({success:false, err});
+            console.log(err);
+            res.status(500).json({ err: 'There was an error while saving the post to the database'});
         });
 });
+
+
 
 router.post('/:id/comments', (req, res) => {
     const postInfoId = req.body;
     const {id} = req.params;
     db.insertComment(id, postInfoId)
         .then(comment => {
-            res.status(201).json({success:true, comment})
+            res.status(201).json({success:true, comment});
         })
         .catch(err => {
             res.status(500).json({success:false, err});
@@ -63,7 +102,7 @@ router.delete('/:id', (req, res) => {
                 err: 'The post could not be removed'
             });
         });
-})
+});
 
 router.put('/:id', (req, res) => {
     const {id} = req.params;
@@ -80,7 +119,7 @@ router.put('/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({success:false, err});
         });
-})
+});
 
 router.patch('/:id', (req, res) => {
     const {id} = req.params;
@@ -97,5 +136,5 @@ router.patch('/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({success:false, err});
         });
-})
+});
 module.exports = router;
